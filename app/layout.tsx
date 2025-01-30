aaf4b70c93c38475433793178f35dcf7e0a1704f
Lior S. <79309515+DeveloperCron@users.noreply.github.com>
2025-01-30 22:05:54 +0200

    Added speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import "./globals.css";
 import { Provider } from "@/components/ui/provider";
@@ -35,6 +36,7 @@ export default function RootLayout({
           <AppNavbar />
           <main>{children}</main>
           <Analytics />
+          <SpeedInsights />
           <AppFooter />
         </Provider>
       </body>
