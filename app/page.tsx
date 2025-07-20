8d9bb1f8eff1e7b75161b56a09f2289a70a87fd7
Kfiros <nevo.kfir@gmail.com>
2025-07-20 18:58:45 +0300

    Refactor Home component; replace Box with SponsorsWrapper for improved structure and styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import {Box, Center, Heading, Image as ChakraImage, Text, VStack} from "@chakra-ui/react";
+import {Center, Image as ChakraImage, Text, VStack} from "@chakra-ui/react";
 import Image from "next/image";
 import React from "react";
 
@@ -11,6 +11,7 @@ import {AccordionItem, AccordionItemContent, AccordionItemTrigger, AccordionRoot
 import {TeamHighlights} from "@/components/home/TeamHighlights";
 import HeroSection from "@/components/home/HeroSection";
 import styled from "styled-components";
+import { colors } from "@/styles/vars";
 
 const HomeWrapper = styled.div`
 	width: 100%;
@@ -18,49 +19,61 @@ const HomeWrapper = styled.div`
 	display: flex;
 	flex-direction: column;
 `;
+
+const SponsorsWrapper = styled.div`
+	width: 100%;
+	margin: 2.5rem 0 1.25rem;
+	justify-content: center;
+	text-align: center;
+	
+	> ._title {
+		font-size: 2.25rem;
+		font-weight: 600;
+		color: ${colors.text.primary};
+	}
+	
+	> ._subtitle {
+		font-size: 1.125rem;
+		color: ${colors.text.secondary};
+		margin-top: 0.5rem;
+	}
+`;
+
 const Home: React.FC = () => {
 	return (
 		<HomeWrapper>
 			<HeroSection />
 			<TeamHighlights/>
-			<Box alignItems={"center"} justifyContent={"center"} justifyItems={"center"} mb={10} mt={5} w={"100%"}>
-				<Center>
-					<VStack spaceY={2}>
-						<VStack>
-							<Heading size={"4xl"}>Our supporters</Heading>
-							<Text color={"gray.400"} textAlign={"center"}>
-								Key companies who provide us everything we need
-							</Text>
-						</VStack>
-						<Carousel>
-							{Sponsors.map((item, index) => (
-								<CarouselCard key={index} cardDescription={item.description} cardImage={item.logo} cardTitle={item.title}/>
-							))}
-						</Carousel>
-						<AccordionRoot collapsible defaultValue={["a"]} hideFrom={"lg"} rounded={"sm"} variant={"subtle"} w={"100%"}>
-							{Sponsors.map((item, index) => (
-								<AccordionItem key={index} value={item.value} w={"100%"}>
-									<AccordionItemTrigger fontSize={"sm"} fontWeight={"medium"} p={2} w={"100%"}>
-										{item.title}
-									</AccordionItemTrigger>
-									<AccordionItemContent p={2}>
-										<Center>
-											<VStack>
-												<ChakraImage asChild height={"auto"} objectFit={"cover"} w={"80%"}>
-													<Image alt={`${item.title}`} src={item.logo}/>
-												</ChakraImage>
-												<VStack>
-													<Text>{item.description}</Text>
-												</VStack>
-											</VStack>
-										</Center>
-									</AccordionItemContent>
-								</AccordionItem>
-							))}
-						</AccordionRoot>
-					</VStack>
-				</Center>
-			</Box>
+			<SponsorsWrapper>
+				<h2 className="_title">Our supporters</h2>
+				<p className="_subtitle">Key companies who provide us everything we need</p>
+				<Carousel>
+					{Sponsors.map((item, index) => (
+						<CarouselCard key={index} cardDescription={item.description} cardImage={item.logo} cardTitle={item.title}/>
+					))}
+				</Carousel>
+				<AccordionRoot collapsible defaultValue={["a"]} hideFrom={"lg"} rounded={"sm"} variant={"subtle"} w={"100%"}>
+					{Sponsors.map((item, index) => (
+						<AccordionItem key={index} value={item.value} w={"100%"}>
+							<AccordionItemTrigger fontSize={"sm"} fontWeight={"medium"} p={2} w={"100%"}>
+								{item.title}
+							</AccordionItemTrigger>
+							<AccordionItemContent p={2}>
+								<Center>
+									<VStack>
+										<ChakraImage asChild height={"auto"} objectFit={"cover"} w={"80%"}>
+											<Image alt={`${item.title}`} src={item.logo}/>
+										</ChakraImage>
+										<VStack>
+											<Text>{item.description}</Text>
+										</VStack>
+									</VStack>
+								</Center>
+							</AccordionItemContent>
+						</AccordionItem>
+					))}
+				</AccordionRoot>
+			</SponsorsWrapper>
 		</HomeWrapper>
 	);
 };
