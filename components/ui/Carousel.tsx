c8372d976222f4fec4ed95c576f58ab5f0904908
Kfiros <nevo.kfir@gmail.com>
2025-07-31 02:16:27 +0300

    Refactor Carousel component; replace Chakra UI elements with styled-components for improved styling and structure

--- components/ui/Carousel.tsx
+++ components/ui/Carousel.tsx
@@ -1,13 +1,12 @@
 "use client";
 
-import { Center, chakra, Box, VStack } from "@chakra-ui/react";
 import { AnimatePresence, motion } from "motion/react";
 import {useEffect, useMemo} from "react";
 import Pagination from "@/components/Pagination";
 import {ItemPaginationStore} from "@/stores/ItemPaginationStore";
 import {observer} from 'mobx-react-lite';
-
-const AnimatedDiv = chakra(motion.div);
+import styled from "styled-components";
+import {mQuery} from "@/styles/vars";
 
 const swipeConfidenceThreshold = 10000;
 const swipePower = (offset: number, velocity: number) => {
@@ -18,6 +17,21 @@ interface CarouselProps {
 	children: React.ReactNode[];
 }
 
+const CarouselContainer = styled.div`
+	margin: 0.6rem;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+	gap: 0.5rem;
+	
+	${mQuery.mobile} {
+		display: none;
+	}
+	
+	> ._card {
+		transition-duration: 200ms;
+	}
+`;
 const Carousel: React.FC<CarouselProps> = observer(({ children }) => {
 	const itemPaginationStore = useMemo(() => new ItemPaginationStore(), []);
 	const {setItems, currentPage, nextPage, prevPage} = itemPaginationStore;
@@ -27,36 +41,32 @@ const Carousel: React.FC<CarouselProps> = observer(({ children }) => {
 	}, []);
 
 	return (
-		<Box hideBelow={"lg"} m={2}>
-			<Center>
-				<VStack>
-					<AnimatePresence mode='wait'>
-						<AnimatedDiv
-							key={currentPage}
-							animate={{ x: 0, opacity: 1 }}
-							drag='x'
-							dragConstraints={{ left: 0, right: 0 }}
-							dragElastic={1}
-							exit={{ x: -300, opacity: 0 }}
-							initial={{ x: 300, opacity: 0 }}
-							transitionDuration={"moderate"}
-							onDragEnd={(e, { offset, velocity }) => {
-								const swipe = swipePower(offset.x, velocity.x);
+		<CarouselContainer>
+			<AnimatePresence mode='wait'>
+				<motion.div
+					key={currentPage}
+					className="_card"
+					animate={{ x: 0, opacity: 1 }}
+					drag='x'
+					dragConstraints={{ left: 0, right: 0 }}
+					dragElastic={1}
+					exit={{ x: -300, opacity: 0 }}
+					initial={{ x: 300, opacity: 0 }}
+					onDragEnd={(e, { offset, velocity }) => {
+						const swipe = swipePower(offset.x, velocity.x);
 
-								if (swipe < -swipeConfidenceThreshold) {
-									nextPage();
-								} else if (swipe > swipeConfidenceThreshold) {
-									prevPage();
-								}
-							}}>
-							{children[currentPage]}
-						</AnimatedDiv>
-					</AnimatePresence>
+						if (swipe < -swipeConfidenceThreshold) {
+							nextPage();
+						} else if (swipe > swipeConfidenceThreshold) {
+							prevPage();
+						}
+					}}>
+					{children[currentPage]}
+				</motion.div>
+			</AnimatePresence>
 
-					<Pagination store={itemPaginationStore} />
-				</VStack>
-			</Center>
-		</Box>
+			<Pagination store={itemPaginationStore} />
+		</CarouselContainer>
 	);
 });
 
